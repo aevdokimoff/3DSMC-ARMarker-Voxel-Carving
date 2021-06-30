bc2a23a64728e9fa3e3fabd95b48a8336d7d8334
Artem Evdokimov <artem.evdokimov@tum.de>
2021-06-30 16:38:15 +0200

    Process image instead of video

--- aruco_markers_detection/opecv-test-2/main.cpp
+++ aruco_markers_detection/opecv-test-2/main.cpp
@@ -7,18 +7,6 @@ using namespace std;
 
 int main() {
     
-    VideoCapture cap(0);
-    
-    if (!cap.isOpened()) {
-        cout << "No webcam, using video file" << endl;
-        // change it to your path
-        cap.open("/Users/artem/Projects/3DSMC-ARMarker-Voxel-Carving/aruco_markers_detection/opecv-test-2/MarkerMovie.mp4");
-        if (cap.isOpened() == false) {
-            cout << "No video!" << endl;
-            exit(0);
-        }
-    }
-    
     cv::Mat cameraMatrix, distCoeffs;
     // change it to your path
     cv::FileStorage fs("/Users/artem/Projects/3DSMC-ARMarker-Voxel-Carving/aruco_markers_detection/opecv-test-2/calibration_params.yml", cv::FileStorage::READ);
@@ -30,27 +18,30 @@ int main() {
     std::cout << "\ndist coeffs\n" << distCoeffs << std::endl;
     
     cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-    while (cap.grab()) {
-        cv::Mat frame, frameCopy;
-        cap.retrieve(frame);
-        frame.copyTo(frameCopy);
-        std::vector<int> ids;
-        std::vector<std::vector<cv::Point2f>> corners;
-        cv::aruco::detectMarkers(frame, dictionary, corners, ids);
-        // if at least one marker detected
-        if (ids.size() > 0) {
-            cv::aruco::drawDetectedMarkers(frameCopy, corners, ids);
-            std::vector<cv::Vec3d> rvecs, tvecs; // rotation and translation vectors -> camera pose
-            cv::aruco::estimatePoseSingleMarkers(corners, 0.05, cameraMatrix, distCoeffs, rvecs, tvecs);
-            // draw axis for each marker
-            for(int i=0; i<ids.size(); i++)
-                cv::aruco::drawAxis(frameCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
-        }
-        cv::imshow("out", frameCopy);
-        int key = waitKey(10);
-        if (key == 27) {
-            break;
-        }
+    
+    // change it to your path
+    string image_path = "/Users/artem/Projects/3DSMC-ARMarker-Voxel-Carving/aruco_markers_detection/opecv-test-2/markers_side.jpg";
+    string image_file = samples::findFile(image_path);
+    Mat image = imread(image_file, IMREAD_COLOR);
+    Mat imageCopy;
+    
+    image.copyTo(imageCopy);
+    vector<int> ids;
+    vector<std::vector<cv::Point2f>> corners;
+    aruco::detectMarkers(image, dictionary, corners, ids);
+    // if at least one marker detected
+    if (ids.size() > 0) {
+        aruco::drawDetectedMarkers(imageCopy, corners, ids);
+        vector<cv::Vec3d> rvecs, tvecs; // rotation and translation vectors -> camera pose
+        aruco::estimatePoseSingleMarkers(corners, 0.05, cameraMatrix, distCoeffs, rvecs, tvecs);
+        // draw axis for each marker
+        for(int i=0; i<ids.size(); i++)
+            aruco::drawAxis(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
+    }
+    imshow("Display window", imageCopy);
+    int k = waitKey(0);
+    if(k == 's') {
+        imwrite(image_path, imageCopy);
     }
     
 }
